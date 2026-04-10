e6f5a0d7a5fafb52294331704e423a26aaffa11e
Micheal-kalach1 <nkemtchoum@gmail.com>
2026-04-10 11:45:06 +0100

    Update payments.js

--- assets/js/modules/payments.js
+++ assets/js/modules/payments.js
@@ -1,52 +1,19 @@
 import { supabase } from "../../../config/supabase.js";
 
-// =========================
-// CHARGER COMMANDES À PAYER
-// =========================
-export async function getPendingOrders() {
+/* =========================
+   PAIEMENT
+========================= */
+export async function createPayment(order_id, caissier_id, amount, methode) {
 
-    const { data } = await supabase
-        .from("orders")
-        .select("*")
-        .eq("statut", "en_attente");
+    const { error } = await supabase.from("payments").insert([
+        {
+            order_id,
+            caissier_id,
+            amount,
+            methode,
+            statut: "valide"
+        }
+    ]);
 
-    return data;
-}
-
-// =========================
-// ENCAISSER UNE COMMANDE
-// =========================
-export async function validatePayment(order_id, caissier_id, methode) {
-
-    // 1. récupérer commande
-    const { data: order } = await supabase
-        .from("orders")
-        .select("*")
-        .eq("id", order_id)
-        .single();
-
-    if (!order) return "Commande introuvable";
-
-    // 2. créer paiement
-    const { error: payError } = await supabase
-        .from("payments")
-        .insert([
-            {
-                order_id,
-                caissier_id,
-                amount: order.total,
-                methode,
-                statut: "valide"
-            }
-        ]);
-
-    if (payError) return payError.message;
-
-    // 3. mettre à jour commande
-    await supabase
-        .from("orders")
-        .update({ statut: "paye" })
-        .eq("id", order_id);
-
-    return "PAIEMENT VALIDÉ";
+    return error ? error.message : "SUCCESS";
 }
